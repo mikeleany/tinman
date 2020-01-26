973442f815af9e66247f37299aaf713ff3a4944c
Mike Leany <55358344+mikeleany@users.noreply.github.com>
2020-01-25 17:38:40 -0700

    Update version number and some copyright dates.

--- src/bin/engine.rs
+++ src/bin/engine.rs
@@ -1,3 +1,12 @@
+//! The tinman chess engine.
+//
+//  Copyright 2020 Michael Leany
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
 use std::fs::File;
 use simplelog::{WriteLogger, LevelFilter, Config};
 use tinman::engine::Engine;
--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,6 +1,6 @@
 //! Supported chess protocols
 //
-//  Copyright 2019 Michael Leany
+//  Copyright 2020 Michael Leany
 //
 //  This Source Code Form is subject to the terms of the Mozilla Public
 //  License, v. 2.0. If a copy of the MPL was not distributed with this
