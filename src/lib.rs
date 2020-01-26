058f9537d62fe4d9573a687adcf0c98170f9ac87
Mike Leany <55358344+mikeleany@users.noreply.github.com>
2020-01-26 08:45:13 -0700

    Update some documentation.
    Also, rename src/bin/engine.rs to src/main.rs so the executable will be named
    tinman instead of engine.

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,21 @@
 //! It is a re-write of my old chess engine, [Vapor](http://vapor.mikeleany.com/), into the Rust
 //! programming language.
 //
-//  Copyright 2019 Michael Leany
+//  Copyright 2020 Michael Leany
 //
 //  This Source Code Form is subject to the terms of the Mozilla Public
 //  License, v. 2.0. If a copy of the MPL was not distributed with this
 //  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-//! # The Executable
-//! The executable has not yet been developed. I am currently working on developing the core
-//! functionality. To learn more about that, see the modules below.
+//! # Execution
+//! To run the engine, you will probably want some kind of user interface. Once the arbiter tool is
+//! completed, it can be used to run automated matches without a graphical interface. If you want
+//! a graphical user interface, for instance to play human vs computer game, you will need one that
+//! is compatible with the xboard protocol, such as
+//! [XBoard/WinBoard](https://www.gnu.org/software/xboard/) or
+//! [Arena](http://www.playwitharena.de/). Refer to the documentation for the specific user
+//! interface for instructions on how to set up new engines. You will need to give the interface
+//! the command to run which is simply the path to the executable.
 //!
 //! # Additional Tools
 //!  - `arbiter` will be a command-line tool meant to facilitate games between two chess engines.
